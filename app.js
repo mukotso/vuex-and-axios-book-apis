859213afc2f2a28d38e7f0f8a9357dbf90015787
Mukotso <kelvinmukotso@gmail.com>
2022-01-20 13:55:41 +0300

    tailwind error solve

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 require('./assets/scss/index.scss')
 require('./assets/css/index.css')
+require ('./src/assets/tailwind.css')
 import './components'
 import router from "./router/index";
 
