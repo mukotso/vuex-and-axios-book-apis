462ffc04533f62370d54e40d5619b862ac144dde
Mukotso <kelvinmukotso@gmail.com>
2022-01-20 14:03:47 +0300

    tailwind changes

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 import Vue from "vue";
 require('./assets/scss/index.scss')
 require('./assets/css/index.css')
-require ('./src/assets/tailwind.css')
 import './components'
 import router from "./router/index";
 
